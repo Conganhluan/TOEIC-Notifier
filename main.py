1b2a4c67307e03e6bb3e7e6943e24199221af8eb
Nguyen Cong Anh Luan <conganhluan@gmail.com>
2023-06-12 00:33:45 +0700

    local test (without sending email) success

--- main.py
+++ main.py
@@ -1,67 +1,68 @@
 import requests
-import sys
+import json
+from os.path import exists, join, dirname
+from os import mkdir
 from bs4 import BeautifulSoup as BS
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# Read information about TOEIC (Reading and Listening)
-response = requests.get("https://iigvietnam.com/lich-thi/?test_type=&location=&start_date=09/01/2023&end_date=10/01/2023")
-content = BS(response.content, "lxml")
+# Preparation with the list of notifications to do
+informationFile =  open(join(dirname(__file__), "information.json"), "r", encoding="UTF-8")
+taskList = json.load(informationFile)
+informationFile.close()
+    
+# Notify news for each person's wanted exams
+for person in taskList:
+    if not exists(join(dirname(__file__), "users", person["name"])):
+        mkdir(join(dirname(__file__), "users", person["name"]))
+    
+    for exam in person["exams"]:
+        
+        # Preparation with needed list
+        saved_tests = []        # saved in file 
+        read_tests = []         # newly read
+        new_tests = []          # not in file
+        
+        # Get the saved dates
+        if exists(join(dirname(__file__), "users", person["name"], str(exam["id"]) + ".txt")):
+            readFile =  open(join(dirname(__file__), "users", person["name"], str(exam["id"]) + ".txt"), "r", encoding="UTF-8")
+            saved_tests = readFile.readlines()
+            readFile.close()
 
-    # Read and find desired information
-finder = content.find_all("td")
-read_dates = []
-for index in range(len(finder)):
-    if finder[index].string == "TOEIC":
-        read_dates.append(finder[index+1].string)
-        index += 1
+        # Get the new read dates and compare to saved dates to find the lately update dates
+        url = "https://iigvietnam.com/lich-thi/?test_type=" + str(exam["id"]) + "&location=" + str(exam["location id"]) + "&start_date=" + exam["start date"] + "&end_date=" + exam["end date"]
+        reader = BS(requests.get(url).content, "lxml").find_all("td")
+        
+        index = 1
+        for idx_tag in range(len(reader)):
+            if (reader[idx_tag].string == str(index)):
+                test = str(reader[idx_tag+1].string) + ' - ' + str(reader[idx_tag+2].string) + '\n'
+                if test not in saved_tests:
+                    new_tests.append(test)
+                    saved_tests.append(test)
+                index += 1
+        
+        # Update the list of saved exam
+        writeFile = open(join(dirname(__file__), "users", person["name"], str(exam["id"]) + ".txt"), "w", encoding="UTF-8")
+        for test in saved_tests:
+            writeFile.write(test)
+        writeFile.close()
 
-    # Read saved dates from file
-with open("TOEIC_dates.txt", "r", encoding="UTF-8") as file:
-    saved_dates = file.readlines()
-    file.close()
+        # Send email
+        '''
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Lately updated schedule for the exam " + str(exam["id"])
+        msg['From'] = "TOEIC Notifier"
+        msg['To'] = person["name"]
 
-    # Compare the read dates with saved dates, update with new dates
-new_dates_TOEIC = []
-for date in read_dates:
-    if date not in saved_dates:
-        new_dates_TOEIC.append(date)
-        saved_dates.append(date)
+        content = ""
+        for exam in new_exams:
+            content = content + exam + "\n"
 
-    # Update the file
-with open("TOEIC_dates.txt", "w", encoding="UTF-8") as file:
-    for line in saved_dates:
-        file.write(line + "\n")
-    file.close()
-
-# Read information about TOEIC SW (Reading and Listening)
-response = requests.get("https://iigvietnam.com/lich-thi/?test_type=94&location=205&start_date=09/01/2023&end_date=10/01/2023")
-content = BS(response.content, "lxml")
-
-    # Read and find desired information
-finder = content.find_all("td")
-read_dates = []
-for index in range(len(finder)):
-    if finder[index].string == "TOEIC SW":
-        read_dates.append(finder[index+1].string)
-        index += 1
-
-    # Read saved dates from file
-with open("TOEIC_SW_dates.txt", "r", encoding="UTF-8") as file:
-    saved_dates = file.readlines()
-    file.close()
-
-    # Compare the read dates with saved dates, update with new dates
-new_dates_TOEIC_SW = []
-for date in read_dates:
-    if date not in saved_dates:
-        new_dates_TOEIC_SW.append(date)
-        saved_dates.append(date)
-
-    # Update the file
-with open("TOEIC_SW_dates.txt", "w", encoding="UTF-8") as file:
-    for line in saved_dates:
-        file.write(line + "\n")
-    file.close()
-
+        body = MIMEText(content, 'plain', "UTF-8")
+        msg.attach(body)
+        s = smtplib.SMTP("localhost")
+        s.sendmail("toeic-notifier@conganhluan.com", person["email"], msg.as_string())
+        s.quit()
+        '''
